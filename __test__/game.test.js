3e7d4c3ef07c81dfc61dd7af8cac26abc64741b6
Getulio Rodriguez <getrojob@gmail.com>
2022-12-01 23:30:38 -0300

    Update game.test.js

--- __test__/game.test.js
+++ __test__/game.test.js
@@ -1,14 +1,12 @@
 const Game = require('../src/game').default
 const fs = require('fs')
 
-jest.setTimeout(30000)
-
 describe('App', () => {
   it('Contains the compiled JavaScript', async (done) => {
     fs.readFile('./public/main.js', 'utf8', (err, data) => {
       expect(err).toBe(null)
-      // expect(data).toMatchSnapshot()
-      // done()
+      expect(data).toMatchSnapshot()
+      done()
     })
   })
 })
